7a65865601da6fe469a667e3225e60f812c0609d
Arnav Johari <arnavjohari123456789@gmail.com>
2026-07-18 09:48:01 +0530

    Refactored WordFreq and Top5Words logic for better modularity

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,46 @@
 import java.util.*;
 
-public static void WordFreq(String input , String output){
+public static Map<String , Integer> WordFreq(String input){
    int ans =0;
+
+   Set<String> excluded = Set.of("is", "am", "are", "the", "of", "for", "and", "not", "if", "with");
+
    String cleanInput = input.replaceAll("[^a-zA-Z\\s]", "").toLowerCase();
    String[] words = cleanInput.split("\\s+");
    HashMap<String , Integer> map = new HashMap<>();
-   for(String word : words){
-      map.put(word , map.getOrDefault(word ,0) + 1);
+   for(String word : words) {
+      if (!excluded.contains(word)) {
+         map.put(word, map.getOrDefault(word, 0) + 1);
+      }
    }
-   if(map.containsKey(output)){
-       ans = map.get(output);
-   } System.out.println(ans);
+   return map;
 }
-public static void main(String[] args){
-   Scanner sc = new Scanner(System.in);
-   String input = sc.nextLine();
-   String output = sc.nextLine();
-   WordFreq(input , output);
+public static void FreqOfRequired(String output , Map<String , Integer> map){
+   if(map.containsKey(output)){
+      System.out.println(map.get(output));
+   }
+}
 
+public static void Top5Words( Map<String , Integer> map){
+   List< Map.Entry<String , Integer>> list =  new ArrayList<>();
+
+   for(Map.Entry<String , Integer> entry : map.entrySet()) {
+      list.add(entry);
+   }
+   list.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+   if(list.size() < 5){
+      System.out.println("Insufficient Words");
+   }
+   for(int i=0;i<Math.min(list.size() , 5);i++){
+      System.out.println(list.get(i));
+   }
+
+}
+public static void main(String[] args) throws IOException{
+   String input = Files.readString(Path.of("input.txt"));
+           Scanner sc = new Scanner(System.in);
+   String output = sc.nextLine();
+  Map<String , Integer> mymap = WordFreq(input);
+  FreqOfRequired (output ,mymap);
+   Top5Words(mymap);
 }
